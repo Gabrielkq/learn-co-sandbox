09e47fec7ea690353ae2b2f0b8bdb0c7791734fc
Gabriel Kutik <gabrielkutik@yahoo.com>
2021-02-06 00:46:34 +0000

    Automatically backed up by Learn

---
+++ learning-comments.rb
@@ -0,0 +1,10 @@
+#puts "joy to the world"
+#puts "all the boys and girls"
+#puts "joy to the fishes in the deep blue sea"
+#puts "joy to you and me"
+run_code_inside = false
+puts "Code before if...end"
+if run_code_inside
+  puts "code inside"
+end
+puts "Code after if...end"
